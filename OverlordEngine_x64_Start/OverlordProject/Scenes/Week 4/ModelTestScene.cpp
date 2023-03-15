cfea905a0b8689264998f6837da90e0770668103
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-03-15 09:08:11 +0100

    W4AX Adding a diffuse material

--- OverlordEngine_x64_Start/OverlordProject/Scenes/Week 4/ModelTestScene.cpp
+++ OverlordEngine_x64_Start/OverlordProject/Scenes/Week 4/ModelTestScene.cpp
@@ -1,13 +1,17 @@
 #include "stdafx.h"
 #include "ModelTestScene.h"
 #include "Materials/ColorMaterial.h"
+#include "Materials/DiffuseMaterial.h"
 
 void ModelTestScene::Initialize()
 {
 	m_pChair = new GameObject();
 	AddChild(m_pChair);
 	auto pColMat = MaterialManager::Get()->CreateMaterial<ColorMaterial>();
+	auto pDiffMat = MaterialManager::Get()->CreateMaterial<DiffuseMaterial>();
 	pColMat->SetColor(XMFLOAT4{ Colors::Red });
+	pDiffMat->SetDiffuseTexture(L"Textures/Chair_Dark.dds");
+	auto& pMat = pDiffMat;
 	auto pModel = m_pChair->AddComponent(new ModelComponent(L"Meshes/Chair.ovm"));
-	pModel->SetMaterial(pColMat->GetMaterialId());
+	pModel->SetMaterial(pMat->GetMaterialId());
 }
