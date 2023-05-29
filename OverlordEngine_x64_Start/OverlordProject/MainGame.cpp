85a88e6071175986ae00beba3c2e88087e714173
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-05-29 11:58:20 +0200

    refactor: minor changes for milestone 2

--- OverlordEngine_x64_Start/OverlordProject/MainGame.cpp
+++ OverlordEngine_x64_Start/OverlordProject/MainGame.cpp
@@ -9,11 +9,11 @@
 //#define W7
 //#define W8
 //#define W9
-#define W10
+//#define W10
 
 /*MILESTONE Content*/
 // #define MILESTONE_1
-// #define MILESTONE_2
+#define MILESTONE_2
 
 #pragma region Lab/Milestone Includes
 #ifdef W3
@@ -85,8 +85,8 @@ void MainGame::OnGamePreparing(GameContext& gameContext)
 	//gameContext.windowWidth=... (default is 1280)
 	//gameContext.windowHeight=... (default is 720)
 
-	gameContext.windowTitle = L"GP2 - Milestone 1 (2023) | (2DAE15) Messely Reï";
-	//gameContext.windowTitle = L"GP2 - Milestone 2 (2023) | (2DAE15) Messely Reï";
+	//gameContext.windowTitle = L"GP2 - Milestone 1 (2023) | (2DAE15) Messely Reï";
+	gameContext.windowTitle = L"GP2 - Milestone 2 (2023) | (2DAE15) Messely Reï";
 	//gameContext.windowTitle = L"GP2 - Exam Project (2023) | (2DAE15) Messely Reï";
 }
 
